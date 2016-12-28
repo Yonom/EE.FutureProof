b5c9d28e72cfb25f16177f24d1e8ebf24e8c6bc8
Sepehr Farshid / Processor <sepehr1376@gmail.com>
2016-12-28 16:45:27 +0100

    Create separate upgrader instances per connection

--- EE.FutureProof.Bridge/IUpgrader.cs
+++ EE.FutureProof.Bridge/IUpgrader.cs
@@ -4,8 +4,6 @@ namespace EE.FutureProof.Bridge
 {
     public interface IUpgrader
     {
-        int FromVersion { get; }
-        int ToVersion { get; }
         Message UpgradeSend(object sender, Message m);
         Message DowngradeReceive(object sender, Message m);
     }
---
+++ EE.FutureProof.Bridge/UpgraderAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EE.FutureProof.Bridge
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public sealed class UpgraderAttribute : Attribute
+    {
+        public int FromVersion { get; private set; }
+        public int ToVersion { get; private set; }
+
+        public UpgraderAttribute(int fromVersion, int toVersion)
+        {
+            this.FromVersion = fromVersion;
+            this.ToVersion = toVersion;
+        }
+    }
+}
