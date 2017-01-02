ab93240a11616fea0690e78a78108410888dabe3
Sepehr Farshid / Processor <sepehr1376@gmail.com>
2017-01-02 12:53:56 +0100

    Implement IConnectionWrapper
    IConnectionWrapper can be used by other libraries to add functionality
    similar to FutureProof

--- EE.FutureProof/FutureProofExtensions.cs
+++ EE.FutureProof/FutureProofExtensions.cs
@@ -5,14 +5,24 @@ namespace EE.FutureProof
     public static class FutureProofExtensions
     {
         private const string GameId = "everybody-edits-su9rn58o40itdbnw69plyw";
-
+        
         public static FutureProofConnection FutureProof(this Connection connection, int fromVersion)
+        {
+            return new PlayerIOConnection(connection).FutureProof(fromVersion);
+        }
+
+        public static FutureProofConnection FutureProof(this Connection connection, int fromVersion, int toVersion)
+        {
+            return new PlayerIOConnection(connection).FutureProof(fromVersion, toVersion);
+        }
+        
+        public static FutureProofConnection FutureProof(this IConnectionWrapper connection, int fromVersion)
         {
             var toVersion = PlayerIO.QuickConnect.SimpleConnect(GameId, "guest", "guest", null).BigDB.Load("Config", "config").GetInt("version");
             return connection.FutureProof(fromVersion, toVersion);
         }
 
-        public static FutureProofConnection FutureProof(this Connection connection, int fromVersion, int toVersion)
+        public static FutureProofConnection FutureProof(this IConnectionWrapper connection, int fromVersion, int toVersion)
         {
             return new FutureProofConnection(connection, FutureProofServices.GetAdapter(fromVersion, toVersion));
         }
